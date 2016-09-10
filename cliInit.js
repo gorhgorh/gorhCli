233a5c5a93c801513b18ec01b5590db001b21638
gorhgorh <gorhgorh@lesnodebots.eu>
2016-09-10 20:27:19 +0200

    first draft of translation cmd

--- cliInit.js
+++ cliInit.js
@@ -24,11 +24,10 @@ const checkFileExistsSync = utils.checkFileExistsSync
  * @returns
  */
 function cliInit () {
-
   // caches the path of the dir where the cli have been inited
   const rcFile = '.gorhClirc'
   const cliDir = process.cwd()
-  //const rcPath = path.join(cliDir, '/', rcFile)
+  // const rcPath = path.join(cliDir, '/', rcFile)
 
   // check for a user config file in os root dir
   const hasUserConfig = checkFileExistsSync(path.join(os.homedir(), '/', rcFile))
@@ -43,7 +42,7 @@ function cliInit () {
     confIsLoaded: false, // flag to check if a rcfile is found
     cliDir, // dir where the cli is invoqued
     rcFile, // name of the rcfile
-    rcPath : cliDir, // defaults to curent dir
+    rcPath: cliDir, // defaults to curent dir
     hasUserConfig, // is there a main config in /home
     initConf: conf // conf loaded via rc
   }
@@ -52,20 +51,18 @@ function cliInit () {
     cliConf.confIsLoaded = true
     cliConf.rcPath = path.dirname(conf.config)
   }
-
+  cliConf.isFromInit = true
   // change cwd to th rc file one if they differ
-  if (_.isEqual(cliConf.cliDir, cliConf.rcPath) !== true){
+  if (_.isEqual(cliConf.cliDir, cliConf.rcPath) !== true) {
     debug(red('cli and rc dir does not match !!'))
     debug(blue('cwd:'), process.cwd())
     try {
-      process.chdir(cliConf.rcPath);
-      debug('new cwd: ' + process.cwd());
-    }
-    catch (err) {
-      debug('chdir: ' + err);
+      process.chdir(cliConf.rcPath)
+      debug('new cwd: ' + process.cwd())
+    } catch (err) {
+      debug('chdir: ' + err)
     }
   }
-
   return cliConf
 }
 
