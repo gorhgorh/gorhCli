8b848535bc38f141a8ca25b44bb59a9ac49f5bfb
gorhgorh <gorhgorh@gmail.com>
2016-09-12 18:31:01 +0200

    added default header for other page in XLS genratiobn for the trad extract CMD

--- tools/makeGaXlsx.js
+++ tools/makeGaXlsx.js
@@ -13,14 +13,59 @@ const blue = chalk.cyan
 // const testData = fs.readJsonSync(path.join(__dirname, '../sandbox/trad/translations/course-00/tradData.json'))
 
 const defHeader = [
-  'reference',
-  'version EN',
-  'version ES',
-  'version EN /* no-markup */',
-  'version ES /* no-markup */',
-  'Translation AXA',
-  'Comments AXA'
+  'REFERENCE',
+  'SOURCE EN',
+  'TARGET ES',
+  'SOURCE EN /* no-markup */',
+  'TARGET ES /* no-markup */',
+  'TRANSLATION AXA',
+  'COMMENTS AXA'
 ]
+
+const defHeaderTr = [
+  'FILE',
+  'TYPE',
+  'TC IN',
+  'TC OUT',
+  'SOURCE',
+  'TARGET',
+  'AXA CORRECTIONS TARGET',
+  'COMMENTS AXA',
+  'TRANS ANSWERS',
+  'WORDCOUNT SOURCE',
+  'WORDCOUNT TARGET',
+  'CHARCOUNT SOURCE',
+  'CHARCOUNT TARGET'
+]
+
+const defHeaderVids = [
+  'FILE',
+  'TYPE',
+  'SOURCE',
+  'TARGET',
+  'AXA CORRECTIONS TARGET',
+  'COMMENTS AXA',
+  'TRANS ANSWERS',
+  'WORDCOUNT SOURCE',
+  'WORDCOUNT TARGET',
+  'CHARCOUNT SOURCE',
+  'CHARCOUNT TARGET'
+]
+
+const defHeaderJPG = [
+  'FILE',
+  'TYPE',
+  'SOURCE',
+  'TARGET',
+  'AXA CORRECTIONS TARGET',
+  'TRANS ANSWERS',
+  'WORDCOUNT SOURCE',
+  'WORDCOUNT TARGET',
+  'CHARCOUNT SOURCE',
+  'CHARCOUNT TARGET'
+]
+
+
 
 function prepareData (data) {
 
@@ -31,11 +76,20 @@ function prepareData (data) {
     const entryText = entry[entryName]
     entryArr.push(entryName)
     entryArr.push(entryText)
-    // debug(entryArr) ;
+    // debug(entryArr)
     return entryArr
   })
   // debug(data.length)
   return dataArr
+}
+
+function fillSheet (data, sheet) {
+  data.forEach(function (entry, i) {
+    entry.forEach(function (data, dI) {
+      sheet.set(dI + 1, i + 1, data)
+    }, this)
+  }, this)
+  return sheet
 }
 
 /**
@@ -45,7 +99,7 @@ function prepareData (data) {
  * @param {String} pth a path to write the xlsx to
  * @param {Array} header an array containing the header's titles
  */
-function createWorkBook (initData, pth,  header) {
+function createWorkBook (initData, pth, header) {
   if (!header) header = defHeader
   const baseName = pth.split('/').pop()
   // Create a new workbook file in current working-path
@@ -56,26 +110,32 @@ function createWorkBook (initData, pth,  header) {
 
   // Create a new worksheet with 10 columns and 12 rows
   var sheet1 = workbook.createSheet('adapt-text', maxCell, maxRow)
-
-  data.forEach(function(entry, i) {
-    entry.forEach(function(data, dI) {
+  var sheet2 = workbook.createSheet('video_transcripts', defHeaderTr.length, maxRow)
+  var sheet3 = workbook.createSheet('texte_overlay_video', defHeaderVids.length, maxRow)
+  var sheet4 = workbook.createSheet('texte_jpg_ou_pdf', defHeaderJPG.length, maxRow)
+  // fill sheet one with data
+  data.forEach(function (entry, i) {
+    entry.forEach(function (data, dI) {
       sheet1.set(dI + 1, i + 1, data)
       if (dI === 1) {
         // debug(data)
-        const cleaned = toTxt.fromString(data, {uppercaseHeadings:false})
+        const cleaned = toTxt.fromString(data, {uppercaseHeadings: false})
           .replace(/\[([^]]+)\]/g, '') // clean images
           .replace(/\n/, '\n ') // add a space before new lines
         sheet1.set(4, i + 1, cleaned)
       }
-    }, this);
-
-  }, this);
-
+    }, this)
+  }, this)
+  // prepare other sheets
+  fillSheet([defHeaderTr], sheet2)
+  fillSheet([defHeaderVids], sheet3)
+  fillSheet([defHeaderJPG], sheet4)
   workbook.save(function (err) {
-    if (err)
+    if (err) {
       throw err
-    else
+    } else {
       debug(blue('xlsx file written in :', pth))
+    }
   })
 }
 // createWorkBook(testData, './')
