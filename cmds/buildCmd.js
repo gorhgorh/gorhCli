19f5daa362398a1f4c5be810f994cf168723a06b
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2016-09-15 12:12:42 +0200

    Restructuration de la commande 'buildCmd'

--- cmds/buildCmd.js
+++ cmds/buildCmd.js
@@ -13,6 +13,8 @@ const sh = require('shelljs')
 const exec = sh.exec
 const which = sh.which
 
+const confMan = require('../confMan')
+const getConf = confMan.getConf
 const utils = require('../utils')
 const getDirsInfo = utils.getDirsInfo
 const buildCourseList = utils.buildCourseList
@@ -26,69 +28,70 @@ function yo (msg, self) {
   self.log(msg + ', yooo')
 }
 
-function Cmd (vorpal, cliConf) {
-  const conf = cliConf.initConf
-  const cliDir = cliConf.rcPath
+function cmdAction (args, cb) {
+  const self = this
+  const opts = args.options
 
-  return vorpal
+  const conf = getConf()
+  let cliDir = process.cwd()
+
+  debug(blue(cmdName, 'start'))
+  debug(opts)
+  // if -c flag clear the builds dir
+  if (opts.clear === true) {
+    debug(blue('cleaning build dir'))
+    fs.removeSync(path.join(cliDir, '/builds'))
+  }
+
+  // clean the build dir
+  fs.removeSync('./build')
+
+  const dirsInfos = getDirsInfo(conf, cliDir)
+  if (getDirsInfo === false) {
+    self.log('invalid config')
+    return cb()
+  }
+
+  debug(blue('dirlistInfos'), dirsInfos)
+
+  // TODO treat list case
+  if (dirsInfos !== false) {
+    if (opts.list === true) {
+      let coursesNames = dirsInfos.fList.existingArr
+      debug(blue('list option'), args.options)
+      self.prompt({
+        type: 'checkbox',
+        name: 'chosenList',
+        message: 'select the dirs you want to build',
+        choices: coursesNames
+      }, function (result) {
+        if (result.chosenList.length < 1) {
+          self.log('no dir selected')
+          return cb()
+        }
+        dirsInfos.fList.existingArr = result.chosenList
+        buildCourseList(dirsInfos, self)
+        if (opts.yo === true) yo('builds done', self)
+        // return cb()
+      })
+    } else {
+      buildCourseList(dirsInfos, self)
+      if (opts.yo === true) yo('builds done', self)
+      // return cb()
+    }
+  } else {
+    debug('dirsinfo = false')
+    self.log('no available dir to build')
+    cb()
+  }
+}
+
+module.exports = function (vorpal, options) {
+  vorpal
     .command('build', cmdMsg)
     .alias('b')
     .option('-l, --list', 'select a list of dirs')
     .option('-c, --clear', 'clear the builds directory beforehand')
     .option('-y, --yo', 'yooo mode ! (hearphones needed)')
-    .action(function (args, cb) {
-      const self = this
-      const opts = args.options
-      debug(blue(cmdName, 'start'))
-      debug(opts)
-      // if -c flag clear the builds dir
-      if (opts.clear === true) {
-        debug(blue('cleaning build dir'))
-        fs.removeSync(path.join(cliDir, '/builds'))
-      }
-
-      // clean the build dir
-      fs.removeSync('./build')
-
-      const dirsInfos = getDirsInfo(conf, cliDir)
-      if (getDirsInfo === false) {
-        self.log('invalid config')
-        return cb()
-      }
-
-      debug(blue('dirlistInfos'), dirsInfos)
-
-      // TODO treat list case
-      if (dirsInfos !== false) {
-        if (opts.list === true) {
-          let coursesNames = dirsInfos.fList.existingArr
-          debug(blue('list option'), args.options)
-          self.prompt({
-            type: 'checkbox',
-            name: 'chosenList',
-            message: 'select the dirs you want to build',
-            choices: coursesNames
-          }, function (result) {
-            if (result.chosenList.length < 1) {
-              self.log('no dir selected')
-              return cb()
-            }
-            dirsInfos.fList.existingArr = result.chosenList
-            buildCourseList(dirsInfos, self)
-            if (opts.yo === true) yo('builds done', self)
-            // return cb()
-          })
-        } else {
-          buildCourseList(dirsInfos, self)
-          if (opts.yo === true) yo('builds done', self)
-          // return cb()
-        }
-      } else {
-        debug('dirsinfo = false')
-        self.log('no available dir to build')
-        cb()
-      }
-    })
+    .action(cmdAction)
 }
-
-module.exports = {Cmd}
